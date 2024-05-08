52e55bd49fd561078985d03eb1b2d10bc1556209
Jorge Jiménez <154094092+JorgeAJT@users.noreply.github.com>
2024-05-08 20:44:25 +0000

    working get, post and delete of every class

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,117 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+# USER FUNCTIONS
+@app.route('/users', methods=['GET'])
+def get_users():
+    all_users = User.query.all()
+    all_users_list = list(map(lambda user: user.serialize(),all_users))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(all_users_list), 200
 
-    return jsonify(response_body), 200
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify(user.serialize()), 200
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    user_data = request.json
+    required_properties = ["email", "first_name", "last_name", "password"]
+
+    for prop in required_properties:
+        if prop not in user_data: return f"The '{prop}' property of the user was not properly written", 400 
+
+    user_to_add = User(**user_data)
+    db.session.add(user_to_add)
+    db.session.commit()
+
+    return "User created properly", 200
+
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def del_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    user_to_delete = user.serialize()
+    user_name = user_to_delete["first_name"]
+    db.session.delete(user)
+    db.session.commit()
+
+    return f"The user '{user_name}' deleted successfully", 200
+
+# CHARACTER FUNCTIONS
+@app.route('/characters', methods=['GET'])
+def get_characters():
+    all_characters = Character.query.all()
+    all_characters_list = list(map(lambda character: character.serialize(),all_characters))
+
+    return jsonify(all_characters_list), 200
+
+@app.route('/characters/<int:character_id>', methods=['GET'])
+def get_character(character_id):
+    character = Character.query.filter_by(id=character_id).first()
+    return jsonify(character.serialize()), 200
+
+@app.route('/characters', methods=['POST'])
+def add_character():
+    character_data = request.json
+    required_properties = ["name", "specie", "height", "gender"]
+
+    for prop in required_properties:
+        if prop not in character_data: return f"The '{prop}' property of the Character was not properly written", 400 
+
+    character_to_add = Character(**character_data)
+    db.session.add(character_to_add)
+    db.session.commit()
+
+    return "Character created properly", 200
+
+@app.route('/characters/<int:character_id>', methods=['DELETE'])
+def del_character(character_id):
+    character = Character.query.filter_by(id=character_id).first()
+    character_to_delete = character.serialize()
+    character_name = character_to_delete["name"]
+    db.session.delete(character)
+    db.session.commit()
+
+    return f"The character '{character_name}' disappeared into the galaxy", 200
+
+# PLANET FUNCTIONS
+@app.route('/planets', methods=['GET'])
+def get_planets():
+    all_planets = Planet.query.all()
+    all_planets_list = list(map(lambda planet: planet.serialize(),all_planets))
+
+    return jsonify(all_planets_list), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.filter_by(id=planet_id).first()
+
+    return jsonify(planet.serialize()), 200
+
+@app.route('/planets', methods=['POST'])
+def add_planets():
+    planet_data = request.json
+    required_properties = ["name", "diameter", "terrain", "population"]
+
+    for prop in required_properties:
+        if prop not in planet_data: return f"The '{prop}' property of the Planet was not properly written", 400 
+
+    planet_to_add = Planet(**planet_data)
+    db.session.add(planet_to_add)
+    db.session.commit()
+
+    return "Planet created properly", 200
+
+@app.route('/planets/<int:planet_id>', methods=['DELETE'])
+def delete_planet(planet_id):
+    planet = Planet.query.filter_by(id=planet_id).first()
+    planet_to_delete = planet.serialize()
+    planet_name = planet_to_delete["name"]
+    db.session.delete(planet)
+    db.session.commit()
+
+    return f"The planet '{planet_name}' was destroyed for the Empire successfully", 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -6,14 +6,57 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    first_name = db.Column(db.String(120), unique=False, nullable=False)
+    last_name = db.Column(db.String(120), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f"<User {self.email}>"
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "is_active": self.is_active,
             # do not serialize the password, its a security breach
+        }
+    
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    specie = db.Column(db.String(50), unique=False, nullable=False)
+    height = db.Column(db.String(20), unique=False, nullable=False)
+    gender = db.Column(db.String(20), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f"<Character {self.id}>"
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "specie": self.specie,
+            "height": self.height,
+            "gender": self.gender,
+            "id": self.id,
+        }
+    
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    population = db.Column(db.String(50), unique=False, nullable=False)
+    terrain = db.Column(db.String(100), unique=False, nullable=False)
+    diameter = db.Column(db.String(50), unique=False, nullable=False)
+
+    def __repr__(self):
+        return f"<Planet {self.id}>"
+
+    def serialize(self):
+        return {
+            "name": self.name,
+            "population": self.population,
+            "terrain": self.terrain,
+            "diameter": self.diameter,
+            "id": self.id,
         }
